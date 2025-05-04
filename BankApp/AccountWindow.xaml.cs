900514bce3385b1f026261d7765941ade39b859e
Wim Bervoets <wim.bervoets@pxl.be>
2025-05-04 15:37:41 +0200

    template

--- BankApp/AccountWindow.xaml.cs
+++ BankApp/AccountWindow.xaml.cs
@@ -22,59 +22,6 @@ namespace BankApp
             InitializeComponent();
         }
 
-        private IBankAccount _account = null;
-
-        private void OnCreateAccount(object sender, RoutedEventArgs e)
-        {
-            CreateAccountWindow window = new CreateAccountWindow();
-
-            if(window.ShowDialog() == true)
-            {
-                MenuItem item = sender as MenuItem;
-                switch(item.Header)
-                {
-                    case "KBC":
-                        _account = new KbcAccount(window.Name, window.Iban);
-                        break;
-                    case "Argenta":
-                        _account = new ArgentaAccount(window.Name, window.Iban);
-                        break;
-                }
-
-                UpdateWindow();
-            }
-        }
-
-        private void UpdateWindow()
-        {
-            bankLabel.Content = _account.Bank;
-            nameLabel.Content = _account.Name;
-            ibanLabel.Content = _account.Iban;
-            saldoLabel.Content = _account.Saldo.ToString("C");
-            amountTextBox.Focus();
-        }
-
-        private void OnAmount_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                if(decimal.TryParse(amountTextBox.Text, out decimal amount))
-                {
-                    if (amount > 0)
-                    {
-                        _account.Deposit(Math.Abs(amount));
-                    }
-                    else
-                    {
-                        _account.Withdrawal(Math.Abs(amount));
-                    }
-
-                    UpdateWindow();
-
-                    amountTextBox.Clear();
-                    amountTextBox.Focus();
-                }
-            }
-        }
+       
     }
 }
